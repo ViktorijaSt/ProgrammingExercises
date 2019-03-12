86c7172ec8930647e645e1a0e458ffab99b4465a
Code <code@sedc.mk>
2019-03-12 19:03:13 +0100

    code

---
+++ ClassSest/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player[] igraci = new Player[]
+            {
+                new Player("Kala", "Kondeva",  0),
+                new Player("Lena", "Stefanova", 0),           
+            };
+            //new Player.FullName();
+            Console.ReadLine();
+        }
+    }
+}
